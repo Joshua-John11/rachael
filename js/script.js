e35c12f9e5873e2d2a6ee2702acea50037cd9199
Joshua John <joshuabaridakarajohn@gmail.com>
2024-08-06 17:21:54 +0100

    Rachael

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation*/
+var typed = new Typed(".typing", {
+    strings:["","Web Designer", "web Developer", "Software Engineer", "UI-UX Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true 
+}) 
